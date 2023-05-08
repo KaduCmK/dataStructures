9f2b72b5988f35ef5cb7a572c775c9785fbc2e22
Carlos Eduardo <kaduu5050@outlook.com>
2023-05-08 02:19:04 -0300

    linkedList - implementado newList() e pop()

--- linkedList/linkedlist.c
+++ linkedList/linkedlist.c
@@ -12,6 +12,19 @@ typedef struct lista {
     int tamanho;
 } Lista;
 
+
+// funcao p instanciar uma lista vazia
+Lista * linkedList() {
+    Lista *l = (Lista*) malloc(sizeof(Lista));
+    l->inicio = NULL;
+    l->fim = NULL;
+    l->tamanho = 0;
+
+    return l;
+}
+
+// O(n)
+// Percorre toda a lista e exibe todos os seus elementos, separados por espaço
 void display(Lista *lista) {
     Node *n = lista->inicio;
     while (n != NULL) {
@@ -73,21 +86,37 @@ void remover(Lista *lista, int index) {
     lista->tamanho--;
 }
 
+// O(n)
+// Percorre toda a lista ate o penultimo elemento;
+// Elimina o ultimo elemento e atribui o penulimo ao ponteiro do fim da lista
+void pop(Lista *lista) {
+    Node *prev = lista->inicio;
+    for (int i = 1; i < lista->tamanho - 1; i++) {
+        prev = prev->proximo;
+    }
+    prev->proximo = NULL;
+    free(lista->fim);
+    lista->fim = prev;
+    lista->tamanho--;
+}
+
 int main() {
-    Lista *l1 = (Lista*) malloc(sizeof(Lista));
-    l1->inicio = NULL;
-    l1->fim = NULL;
-    l1->tamanho = 0;
+    Lista *l1 = linkedList();
 
     append(l1, 1);
     append(l1, 2);
     append(l1, 3);
     append(l1, 4);
     remover(l1, 3);
-    pop(l1);
 
+    append(l1, 50);
     display(l1);
+    pop(l1);
+    display(l1);
+
     printf("%d", l1->tamanho);
+
+    free(l1);
 
     return 0;
 }
